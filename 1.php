58f4898badc81c03790f39c48cef3f43b2253444
Soroush2 <sorush365@gmail.com>
2022-11-09 18:41:49 +0330

    perfect or not

--- 1.php
+++ 1.php
@@ -24,6 +24,7 @@ echo "جمع ارقام:"."<br>";
       $num /=10;
       
    }
+   unset($i);
    echo $sum."<br>"; 
 //#2
 echo "زوج یا فرد بودن :"."<br>";
@@ -35,7 +36,20 @@ echo "زوج یا فرد بودن :"."<br>";
       echo "It's odd"."<br>";
    }
 //#3
-
+echo "اول بودن:"."<br>";
+$n=21;
+$counter=0;
+for($i=1;$i<$n;$i++){
+   if($n%$i==0){
+      $counter++;
+   }
+}
+if($counter<=1){
+   echo $n." is a perfect number ";
+}
+else{
+   echo $n. " is not a perfect number";
+}
 ?>
 </body>
 
